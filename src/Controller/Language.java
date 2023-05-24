79545332404ac4f94549a2b543dbbef2a62feb49
George <alupop7602@ieselcaminas.org>
2023-05-24 18:27:44 +0200

    ª

---
+++ src/Controller/Language.java
@@ -0,0 +1,34 @@
+package Controller;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class Language extends Properties{
+    public static final int spanish = 0;
+    public static final int english = 1;
+    public Language(int lang){
+        switch(lang){
+            case spanish:
+                getProperties("/espanol.properties");
+                break;
+            case english:
+                getProperties("/english.properties");
+                break;
+            default:
+                getProperties("/espanol.properties");
+        }
+    }
+
+    private void getProperties(String langFile) {
+        try {
+            this.load( getClass().getResourceAsStream(langFile) );
+
+        } catch (IOException ignored) {
+
+        }
+    }
+    public void changeLanguage(int newLang){
+        Language language = new Language(newLang);
+
+    }
+}
